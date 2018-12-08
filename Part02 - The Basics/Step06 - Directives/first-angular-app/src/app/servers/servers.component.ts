de68d17c2121a92844910573272bc7c48960060b
Unknown <khaufra.maggini@gmail.com>
2018-12-08 12:41:41 +0100

    Part 02 Step 06 - Directives

--- Part02 - The Basics/Step06 - Directives/first-angular-app/src/app/servers/servers.component.ts
+++ Part02 - The Basics/Step06 - Directives/first-angular-app/src/app/servers/servers.component.ts
@@ -11,6 +11,7 @@ export class ServersComponent implements OnInit {
   serverCreationStatus: string = 'No server was created!';
   private serverName: string = 'TestServer';
   serverCreated: boolean = false;
+  servers = ['TestServer', 'TestServer 2'];
 
   constructor() {
     setTimeout(() => {
@@ -23,6 +24,7 @@ export class ServersComponent implements OnInit {
 
   onCreateServer(){
     this.serverCreated = true;
+    this.servers.push(this.serverName);
     this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
   }
 
