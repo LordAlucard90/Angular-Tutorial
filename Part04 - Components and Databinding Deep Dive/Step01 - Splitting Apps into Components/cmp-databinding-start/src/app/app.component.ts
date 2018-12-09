9c11e8c5b9c7c2c9c809018a942c66e4d89b9957
Unknown <khaufra.maggini@gmail.com>
2018-12-09 12:51:58 +0100

    Part 04 Step 01 - Splitting Apps into Components

--- Part04 - Components and Databinding Deep Dive/Step01 - Splitting Apps into Components/cmp-databinding-start/src/app/app.component.ts
+++ Part04 - Components and Databinding Deep Dive/Step01 - Splitting Apps into Components/cmp-databinding-start/src/app/app.component.ts
@@ -6,23 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  serverElements = [];
-  newServerName = '';
-  newServerContent = '';
+  serverElements = [{type: 'server', name: 'Test Server', content: 'Dummy Content'}];
 
-  onAddServer() {
+  onServerAdded(serverData: {serverName: string, serverContent: string}) {
     this.serverElements.push({
       type: 'server',
-      name: this.newServerName,
-      content: this.newServerContent
+      name: serverData.serverName,
+      content: serverData.serverContent
     });
   }
 
-  onAddBlueprint() {
+  onBlueprintAdded(blueprintData: {serverName: string, serverContent: string}) {
     this.serverElements.push({
       type: 'blueprint',
-      name: this.newServerName,
-      content: this.newServerContent
+      name: blueprintData.serverName,
+      content: blueprintData.serverContent
     });
   }
 }
---
+++ Part04 - Components and Databinding Deep Dive/readme.md
@@ -0,0 +1,5 @@
+# Part 04 - Components and Databinding Deep Dive
+
+- **Step01 - Splitting Apps into Components**
+  - Split intro, Custom Property Binding, Custom Event Binding.
+
