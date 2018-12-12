4718c2aaeae9050b6029a4ec601678fd1d5951ac
Unknown <khaufra.maggini@gmail.com>
2018-12-12 11:17:24 +0100

    Part04 Step 04 - Assignment 04

--- Part04 - Components and Databinding Deep Dive/Step04 - Assignment04/assignment-four/src/app/app.component.ts
+++ Part04 - Components and Databinding Deep Dive/Step04 - Assignment04/assignment-four/src/app/app.component.ts
@@ -1,9 +1,20 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Output} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
+  oddsEmitted: number[] = [];
+  evensEmitted: number[] = [];
+
+  onNumberEmitted(emittedNumber: number) {
+    if (emittedNumber % 2 === 0){
+      this.evensEmitted.push(emittedNumber);
+    } else {
+      this.oddsEmitted.push(emittedNumber);
+    }
+  }
 }
--- Part04 - Components and Databinding Deep Dive/Step04 - Assignment04/assignment-four/src/app/app.module.ts
+++ Part04 - Components and Databinding Deep Dive/Step04 - Assignment04/assignment-four/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { OddComponent } from './odd/odd.component';
+import { EvenComponent } from './even/even.component';
+import { GameControlComponent } from './game-control/game-control.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OddComponent,
+    EvenComponent,
+    GameControlComponent
   ],
   imports: [
     BrowserModule
