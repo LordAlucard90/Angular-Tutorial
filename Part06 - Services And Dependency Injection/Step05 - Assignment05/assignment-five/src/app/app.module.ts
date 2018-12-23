d5e6653794eab703b6831b593ed323a363ed0868
Unknown <khaufra.maggini@gmail.com>
2018-12-23 11:01:40 +0100

    Part 06 Step 05 - Assignment 05 - Start

---
+++ Part06 - Services And Dependency Injection/Step05 - Assignment05/assignment-five/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import {ActiveUsersComponent} from "./active-users/active-users.component";
+import {InactiveUsersComponent} from "./inactive-users/inactive-users.component";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ActiveUsersComponent,
+    InactiveUsersComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ Part06 - Services And Dependency Injection/Step05 - Assignment05/readme.md
@@ -0,0 +1,7 @@
+# Step 05 - Assignment 05
+
+## Exercise
+
+- Optimize this app by adding a UsersService which manages the active and inactive users.
+
+- Also add a CounterService which counts the number of active->inactive and inactive->active actions.
