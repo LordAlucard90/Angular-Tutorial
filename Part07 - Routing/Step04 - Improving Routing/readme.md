ade9ec5d8aafd466f4c12c5bc1dfc379f2610aac
Unknown <khaufra.maggini@gmail.com>
2018-12-27 13:47:43 +0100

    Part 07 Step 04 - Outsourcing Routes

--- Part07 - Routing/Step04 - Improving Routing/readme.md
+++ Part07 - Routing/Step04 - Improving Routing/readme.md
@@ -1,3 +1,35 @@
-## Step 04 - Improving Routing
+# Step 04 - Improving Routing
+
+## Outsourcing Routes
+
+To keep all the routes ordered it is possible move them in a module **app-routing.module.ts** :
+
+```typescript
+import ...;
+
+const appRoutes: Routes = [...];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(appRoutes)
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {
+}
+```
+
+And import the new module in the **app.mosule.ts**:
+
+```typescript
+@NgModule({
+  ...,
+  imports: [
+    ...,
+    AppRoutingModule
+  ],
+  ...
+})
+```
 
 
--- Part07 - Routing/Step04 - Improving Routing/routing-start/src/app/app.module.ts
+++ Part07 - Routing/Step04 - Improving Routing/routing-start/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {RouterModule, Routes} from '@angular/router';
 import {FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
@@ -11,20 +10,8 @@ import {UserComponent} from './users/user/user.component';
 import {ServerComponent} from './servers/server/server.component';
 import {EditServerComponent} from './servers/edit-server/edit-server.component';
 import {ServersService} from './servers/servers.service';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-
-const appRoutes: Routes = [
-  { path: '', component: HomeComponent},
-  { path: 'users', component: UsersComponent, children: [
-    { path: ':id/:name', component: UserComponent },
-    ] },
-  { path: 'servers', component: ServersComponent, children: [
-    { path: ':id', component: ServerComponent },
-    { path: ':id/edit', component: EditServerComponent },
-    ] },
-  { path: 'not-found', component: PageNotFoundComponent},
-  { path: '**', redirectTo: '/not-found'},
-];
+import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
+import {AppRoutingModule} from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -40,7 +27,7 @@ const appRoutes: Routes = [
   imports: [
     BrowserModule,
     FormsModule,
-    RouterModule.forRoot(appRoutes)
+    AppRoutingModule
   ],
   providers: [ServersService],
   bootstrap: [AppComponent]
