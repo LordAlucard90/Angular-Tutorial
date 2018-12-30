5fddb9b937e62459bfbf1b0be4d61fac7235b6cb
Unknown <khaufra.maggini@gmail.com>
2018-12-30 14:42:55 +0100

    Part 09 Step 03 - Validation

--- Part09 - Forms/Step03 - Validation/forms-td-start/src/app/app.component.ts
+++ Part09 - Forms/Step03 - Validation/forms-td-start/src/app/app.component.ts
@@ -8,9 +8,35 @@ import {NgForm} from '@angular/forms';
 })
 export class AppComponent {
   @ViewChild('f') myForm: NgForm;
+  defaultQuestion = 'pet';
+  answer: string;
+  genders = ['male', 'female'];
+  user = {
+    username: '',
+    email: '',
+    secretQuestion: '',
+    answer: '',
+    gender: ''
+  };
+  submitted = false;
 
   suggestUserName() {
     const suggestedName = 'Superuser';
+    // this.myForm.setValue({
+    //   userData: {
+    //     username: suggestedName,
+    //     email: ''
+    //   },
+    //   secret: 'pet',
+    //   questionAnswer: '',
+    //   gender: 'male'
+    // });
+    this.myForm.form.patchValue({
+      userData: {
+        username: suggestedName,
+      },
+
+    });
   }
 
   // onSubmit(form: HTMLFormElement) {
@@ -20,6 +46,14 @@ export class AppComponent {
   // }
 
   onSubmit() {
-    console.log(this.myForm);
+    // console.log(this.myForm);
+    this.submitted = true;
+    this.user.username = this.myForm.value.userData.username;
+    this.user.email = this.myForm.value.userData.email;
+    this.user.secretQuestion = this.myForm.value.secret;
+    this.user.answer = this.myForm.value.questionAnswer;
+    this.user.gender = this.myForm.value.gender;
+
+    this.myForm.reset();
   }
 }
--- Part09 - Forms/readme.md
+++ Part09 - Forms/readme.md
@@ -8,4 +8,14 @@
   - Registering Form Elements
   - Retrieving Forms
 - **Step03 - Validation**
-
+  - Validators
+  - Submit Control
+  - Styling Validation
+  - Showing Validation Massages
+  - Default Values
+  - Binding Values
+  - Grouping Form Controls
+  - Radio Buttons
+  - Changing Values
+  - Retrieving Input Values
+  - Resetting Form
