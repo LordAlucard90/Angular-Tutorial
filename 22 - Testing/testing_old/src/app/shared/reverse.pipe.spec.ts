b8cc307a0a460130c69f80a38f159eb48e82d629
LordAlucard90 <khaufra.maggini@gmail.com>
2022-06-24 14:29:15 +0200

    removed old code

--- 22 - Testing/testing_old/src/app/shared/reverse.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import {ReversePipe} from './reverse.pipe';
-
-describe('ReversePipe', () => {
-  it('should reverse text', () => {
-    const reversePipe = new ReversePipe();
-    expect(reversePipe.transform('hello')).toEqual('olleh');
-  });
-});
--- 22 - Testing/testing_old/src/app/user/user.service.ts
+++
@@ -1,7 +0,0 @@
-export class UserService {
-  user = {
-    name: 'Ciccio'
-  };
-}
-
-
